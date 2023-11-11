0a5ba88afefe89cbe62a5fd9c7fbe796f8575583
Marianna Zhukova <mzhukova@c2s19d128.42wolfsburg.de>
2023-11-11 14:50:29 +0100

    ex23

--- ex23/ft_point.h
+++ ex23/ft_point.h
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_abs.h                                           :+:      :+:    :+:   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/11 14:02:39 by mzhukova          #+#    #+#             */
-/*   Updated: 2023/11/11 14:48:02 by mzhukova         ###   ########.fr       */
+/*   Created: 2023/11/11 14:31:28 by mzhukova          #+#    #+#             */
+/*   Updated: 2023/11/11 14:50:06 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_ABS_H
-# define FT_ABS_H
 
-int	return_val(int value)
+#ifndef FT_POINT_H
+
+# define FT_POINT_H
+
+typedef struct s_point
 {
-	if (value > 0)
-		return (value);
-	return (-value);
-}
-# define ABS(value)	return_val(value)
+	int	x;
+	int	y;
+}	t_point;
 
 #endif
